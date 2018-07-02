d7af758c7d0900bfe7225b834b507455c3cdbf3b
Rajesh Duggal <rajesh@duggalmail.com>
2018-07-02 15:30:38 -0400

    Take github username from command line

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,12 @@ import { User } from './User';
 import { Repo } from './Repo';
 
 let svc = new GithubApiService();
-svc.getUserInfo('rajeshduggal', (user: User) => {
-    svc.getRepos('rajeshduggal', (repos: Repo[]) => {
+let username: string = 'rajeshduggal';
+if (process.argv.length === 3) {
+    username = process.argv[2];
+}
+svc.getUserInfo(username, (user: User) => {
+    svc.getRepos(username, (repos: Repo[]) => {
         let sortedRepos = _.sortBy(repos,[(repo: Repo) => repo.forkCount * -1]);
         let filteredRepos = _.take(sortedRepos, 3);
         user.repos = filteredRepos;
