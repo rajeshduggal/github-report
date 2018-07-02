d92a1e6711e715e2c8c972754de6784b248c3ae4
Rajesh Duggal <rajesh@duggalmail.com>
2018-07-02 15:05:26 -0400

    Sort repo listing by forkCount

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import {GithubApiService} from './GithubApiService';
+import * as _ from 'lodash';
 import { User } from './User';
 import { Repo } from './Repo';
 
 let svc = new GithubApiService();
 svc.getUserInfo('rajeshduggal', (user: User) => {
     svc.getRepos('rajeshduggal', (repos: Repo[]) => {
-        user.repos = repos;
+        let sortedRepos = _.sortBy(repos,[(repo: Repo) => repo.forkCount * -1]);
+        user.repos = sortedRepos;
         console.log(user);
     })
 });
