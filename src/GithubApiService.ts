a50a7b1d475d98b7f22842f613406fdb98a2569f
Rajesh Duggal <rajesh@duggalmail.com>
2018-07-02 14:39:36 -0400

    GithubApiService callbacks

--- src/GithubApiService.ts
+++ src/GithubApiService.ts
@@ -1,21 +1,28 @@
 import * as request from 'request';
+import { User } from './User';
+import { Repo } from './Repo';
 
 export class GithubApiService {
-    getUserInfo(userName: string) {
-        let options: any = {
-            headers: {
-                'User-Agent': 'request'
-            }
-        }
-        request.get('https://api.github.com/users/' + userName, options,
+    private readonly options: any = {
+        headers: {
+            'User-Agent': 'request'
+        },
+        json: true
+    }
+
+    getUserInfo(userName: string, cb: (user: User) => any ) {
+        request.get('https://api.github.com/users/' + userName, this.options,
             (error: any, response: any, body: any) => {
-                console.log(error);
-                console.log(response);
-                console.log(body);
+                let user = new User(body);
+                cb(user);
         });
     }
 
-    getRepos() {
-
+    getRepos(userName: string, cb: (repos: Repo[]) => any ) {
+        request.get('https://api.github.com/users/' + userName + '/repos', this.options,
+            (error: any, response: any, body: any) => {
+                let repos = body.map((repo: any) => new Repo(repo));
+                cb(repos);
+        });
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
 import {GithubApiService} from './GithubApiService';
+import { User } from './User';
+import { Repo } from './Repo';
 
 let svc = new GithubApiService();
-svc.getUserInfo('rajeshduggal');
+svc.getUserInfo('rajeshduggal', (user: User) => {
+    console.log(user);
+});
+
+svc.getRepos('rajeshduggal', (repos: Repo[]) => {
+    console.log(repos);
+});
